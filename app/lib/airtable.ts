b00c241955ca81663a24d02916e31db3a1f2a65d
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-23 00:43:50 +0700

    feat: airtable fetch with axios

--- app/lib/airtable.ts
+++ app/lib/airtable.ts
@@ -1,8 +1,23 @@
 import Airtable from 'airtable'
+import axios from 'axios'
+
+/**
+ * Airtable SDK
+ */
 
 Airtable.configure({
   endpointUrl: 'https://api.airtable.com',
   apiKey: String(process.env.AIRTABLE_API_KEY),
 })
 
-const base = Airtable.base(String(process.env.AIRTABLE_BASE_ID))
+export const airtableBase = Airtable.base(String(process.env.AIRTABLE_BASE_ID))
+
+/**
+ * Aritable via Fetch/Axios
+ */
+
+export const airtableFetch = axios.create({
+  baseURL: `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}`,
+  timeout: 3000,
+  headers: { Authorization: `Bearer ${String(process.env.AIRTABLE_API_KEY)}` },
+})
