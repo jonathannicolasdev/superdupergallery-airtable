606a6ec0f0c66edaebe0bf59c801719a43042ee0
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-23 00:43:58 +0700

    feat: artworks on the homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,26 @@
 import { json, useLoaderData } from 'remix'
+import { airtableFetch } from '~/lib/airtable'
 
-export function loader() {
-  return json({
-    message: 'Hello',
-  })
+export async function loader() {
+  const artworks = await airtableFetch(
+    '/Artworks?maxRecords=3&view=All%20Artworks'
+  )
+
+  return json(artworks?.data?.records)
 }
 
 export default function Index() {
-  const data = useLoaderData()
+  const artworks = useLoaderData()
 
   return (
     <div>
       <h1>Super Duper Gallery</h1>
-      <p>{JSON.stringify(data)}</p>
+      <ul>
+        {artworks.map((artwork: any) => {
+          return <li key={artwork.id}>{artwork.fields.title}</li>
+        })}
+      </ul>
+      <pre>{JSON.stringify(artworks)}</pre>
     </div>
   )
 }
