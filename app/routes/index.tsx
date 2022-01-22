dab387cc36fff4eed97a7ebc6479fa027a528933
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-23 00:20:28 +0700

    feat: use loader data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,18 @@
+import { json, useLoaderData } from 'remix'
+
+export function loader() {
+  return json({
+    message: 'Hello',
+  })
+}
+
 export default function Index() {
+  const data = useLoaderData()
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Super Duper Gallery</h1>
+      <p>{JSON.stringify(data)}</p>
     </div>
-  );
+  )
 }
