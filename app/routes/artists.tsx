22734fe98ae37f171300077ea12af3aeba9ec20e
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-29 23:20:03 +0700

    feat: artists page

---
+++ app/routes/artists.tsx
@@ -0,0 +1,36 @@
+import { json, useLoaderData } from 'remix'
+import { airtableFetch } from '~/lib/airtable'
+
+export async function loader() {
+  const artists = await airtableFetch(
+    '/Artists?maxRecords=10&view=All%20Artists'
+  )
+
+  return json(artists?.data?.records)
+}
+
+export default function Index() {
+  const artists = useLoaderData()
+
+  return (
+    <div>
+      <h1>Artists</h1>
+      <hr />
+
+      <div>
+        {artists.map((artist: any) => {
+          const { name } = artist.fields
+
+          return (
+            <div key={artist.id}>
+              <h1>{name}</h1>
+            </div>
+          )
+        })}
+      </div>
+
+      <hr />
+      <pre>{JSON.stringify(artists, null, 2)}</pre>
+    </div>
+  )
+}
