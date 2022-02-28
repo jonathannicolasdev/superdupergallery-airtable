38b8e18a696f43b13e9e52c804c2453dd60649cb
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-01 00:30:05 +0700

    feat: get single artist data for $username page

--- app/routes/artists/$username.tsx
+++ app/routes/artists/$username.tsx
@@ -1,46 +1,39 @@
-import { FunctionComponent } from 'react'
 import { json, LoaderFunction, useLoaderData } from 'remix'
 import { Hero, AnimatedHeading, Center } from '~/components'
 import { airtableFetch } from '~/lib'
 import { ArtistContent, ArtworkContent } from '~/types'
 
-// export async function loader() {
-//   const artists = await airtableFetch(
-//     '/Artists?maxRecords=10&view=All%20Artists'
-//   )
-
-//   const transformedArtists = artists?.data?.records.map((artist: any) => {
-//     const { name, username } = artist.fields
-//     return {
-//       name,
-//       username,
-//     }
-//   })
-
-//   console.log(transformedArtists)
-
-//   return json(transformedArtists)
-// }
-
 export const loader: LoaderFunction = async ({ params }) => {
-  const username = params.username
+  const artists = await airtableFetch(
+    '/Artists?maxRecords=10&view=All%20Artists'
+  )
+  const artist = artists?.data?.records.find((artist: any) => {
+    if (params.username === artist.fields.username) {
+      return artist
+    }
+  })
+  const { name, username } = artist.fields
 
-  return {
-    username,
-  }
+  return json({
+    artist: {
+      name,
+      username,
+    },
+    artworks: [],
+  })
 }
 
 export default function ArtistUsernamePage() {
-  const data = useLoaderData()
+  const { artist } = useLoaderData()
 
-  // const url = `/artists/${artist.username}`
-  // const title = artist.name
+  const url = `/artists/${artist.username}`
+  const title = artist.name
 
   return (
     <div>
-      <pre>{JSON.stringify(data)}</pre>
-
-      <Hero>{/* <AnimatedHeading sentence={artist.name} /> */}</Hero>
+      <Hero>
+        <AnimatedHeading sentence={artist.name} />
+      </Hero>
 
       <Center>{/* <ArtworkList artworks={artworks} /> */}</Center>
     </div>
