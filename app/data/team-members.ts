31d91c99f273e4cf8efc0beedb498f4dbcd96135
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-28 22:36:51 +0700

    feat: team members data

---
+++ app/data/team-members.ts
@@ -0,0 +1,20 @@
+export const teamMembersData = [
+  {
+    slug: 'jonathan-nicolas',
+    name: 'Jonathan Nicolas',
+    role: 'Co-Founder',
+    avatarImageURL: '/avatars/jonathan-nicolas.png',
+  },
+  {
+    slug: 'alfred-tababa',
+    name: 'Alfred Tababa',
+    role: 'Co-Founder',
+    avatarImageURL: '/avatars/alfred-tababa.png',
+  },
+  {
+    slug: 'jessa-almirol',
+    name: 'Jessa Almirol',
+    role: 'Co-Founder',
+    avatarImageURL: '/avatars/jessa-almirol.png',
+  },
+]
