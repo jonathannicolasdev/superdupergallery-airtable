4606bb30c0fd6d661ce9c9020209287a32745664
M Haidar Hanif <git@mhaidarhanif.com>
2022-06-07 12:25:49 +0700

    feat: meta opengraph data

--- app/root.tsx
+++ app/root.tsx
@@ -7,53 +7,57 @@ import {
   Scripts,
   ScrollRestoration,
   useTransition,
-} from 'remix'
-import type { MetaFunction } from 'remix'
-import NProgress from 'nprogress'
+} from "remix";
+import type { MetaFunction } from "remix";
+import NProgress from "nprogress";
 
-import { Layout } from '~/components'
-import nProgressStyles from '~/styles/nprogress.css'
-import { useEffect } from 'react'
+import { Layout } from "~/components";
+import nProgressStyles from "~/styles/nprogress.css";
+import { useEffect } from "react";
 
 export const meta: MetaFunction = () => {
   return {
-    title: 'Super Duper Gallery',
-    description: 'Amazing gallery in QC, Philippines',
-  }
-}
+    title: "Super Duper Gallery",
+    description: "Amazing gallery in QC, Philippines",
+    "og:title": "Super Duper Gallery",
+    "og:type": "website",
+    "og:url": "https://superdupergallery.com",
+    "og:image": "/images/superdupergallery-og.jpeg",
+  };
+};
 
 export const links: LinksFunction = () => {
   return [
     {
-      rel: 'preconnect',
-      href: 'https://fonts.googleapis.com',
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
     },
     {
-      rel: 'preconnect',
-      href: 'https://fonts.gstatic.com',
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
       // crossOrigin: true,
     },
     {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Titillium+Web:wght@900&display=swap',
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Titillium+Web:wght@900&display=swap",
     },
     {
-      rel: 'stylesheet',
+      rel: "stylesheet",
       href: nProgressStyles,
     },
-  ]
-}
+  ];
+};
 
 export default function App() {
-  const transition = useTransition()
+  const transition = useTransition();
 
   useEffect(() => {
     // when the state is idle then we can to complete the progress bar
-    if (transition.state === 'idle') NProgress.done()
+    if (transition.state === "idle") NProgress.done();
     // and when it's something else it means it's either submitting a form or
     // waiting for the loaders of the next location so we start it
-    else NProgress.start()
-  }, [transition.state])
+    else NProgress.start();
+  }, [transition.state]);
 
   return (
     <html lang="en">
@@ -71,8 +75,8 @@ export default function App() {
 
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === 'development' && <LiveReload />}
+        {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
-  )
+  );
 }
